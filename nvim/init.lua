7644977f7f6ebcb4a9aa94f9794ef009a258b75a
Valiance <lucas.ludovic@epitech.eu>
2024-09-13 10:07:28 +0200

    [FEATURE] (nvim) Add autocomplete for js

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,3 +40,4 @@ vim.keymap.set("n", "<A-h>", "<C-\\><C-N><C-w>h", { desc = 'move left in normal 
 vim.keymap.set("n", "<A-j>", "<C-\\><C-N><C-w>j", { desc = 'move down in normal mode' })
 vim.keymap.set("n", "<A-k>", "<C-\\><C-N><C-w>k", { desc = 'move up in normal mode' })
 vim.keymap.set("n", "<A-l>", "<C-\\><C-N><C-w>l", { desc = 'move right in normal mode' })
+
